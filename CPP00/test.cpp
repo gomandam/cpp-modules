8b52b1946c10fcad5608e9950cdc615bae28de1f
gomandam <gomandam@student.42madrid.com>
2026-05-19 00:49:50 +0200

    [git] initialize

---
+++ CPP00/test.cpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gomandam <gomandam@student.42madrid>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/18 20:17:12 by gomandam          #+#    #+#             */
+/*   Updated: 2026/05/18 20:57:08 by gomandam         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main(void)
+{
+	std::cout << "Test!\n";
+	return (0);	// Signals program for successful termination
+}
+
+// 	Namespace prefix "std::" prefix tells the compiler to look for "cout" inside 
+// 	standard library namespace. A way to group related names, avoid conflicts.
