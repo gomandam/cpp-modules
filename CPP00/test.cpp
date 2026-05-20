238b6a285297ffc2497abdbb9c4e80f4f9d208bb
gomandam <gomandam@student.42madrid.com>
2026-05-20 18:46:56 +0200

    [add] CPP00

--- CPP00/test.cpp
+++ CPP00/test.cpp
@@ -6,7 +6,7 @@
 /*   By: gomandam <gomandam@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/18 20:17:12 by gomandam          #+#    #+#             */
-/*   Updated: 2026/05/18 20:57:08 by gomandam         ###   ########.fr       */
+/*   Updated: 2026/05/20 18:41:02 by gomandam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,10 @@
 
 int	main(void)
 {
-	std::cout << "Test!\n";
+	std::cout << "program success\n";
 	return (0);	// Signals program for successful termination
 }
 
 // 	Namespace prefix "std::" prefix tells the compiler to look for "cout" inside 
 // 	standard library namespace. A way to group related names, avoid conflicts.
+// COMPILATION: g++ -Wall -Wextra -Werror -std=c++98 test.cpp -o test.out
