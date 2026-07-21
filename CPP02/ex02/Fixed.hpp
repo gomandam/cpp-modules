a92b9f9f4f9fb6b0232066a6c49ff0c35682e26b
gomandam <gomandam@student.42madrid.com>
2026-07-21 02:52:39 +0200

    [add] ex02

---
+++ CPP02/ex02/Fixed.hpp
@@ -0,0 +1,84 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gomandam <gomandam@student.42madrid>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/03 23:08:33 by gomandam          #+#    #+#             */
+/*   Updated: 2026/07/21 02:49:21 by gomandam         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef	FIXED_HPP
+#define	FIXED_HPP
+
+#include <iostream>
+#include <cmath>
+
+class Fixed
+{
+	public:
+// CONSTRUCTORS FROM CONVERSIONS
+		Fixed(const int value);
+		Fixed(const float value); 
+
+// ORTHODOX CANONICAL FORM
+		Fixed(void);				// default constructor
+		~Fixed(void);				// destructor
+		Fixed(const Fixed& other);		// copy constructor
+		Fixed& operator=(const Fixed& other);	// copy assignment destructor
+
+// CONVERSIONS
+	float	toFloat(void) const;		// fixed point value to floating point value
+	int	toInt(void) const;		// fixed point value to an integer value
+
+// ENCAPSULATION AND ACCESSORS	
+	int	getRawBits(void) const;			// gets the FPV from the private class
+	void	setRawBits(int const newValue); 	// sets the raw value
+
+// COMPARISON OPERATORS
+	bool operator>(const Fixed& other) const;
+	bool operator<(const Fixed& other) const;
+	bool operator>=(const Fixed& other) const;
+	bool operator<=(const Fixed& other) const;
+	bool operator==(const Fixed& other) const;
+	bool operator!=(const Fixed& other) const;
+
+// ARITHMETIC OPERATORS
+	Fixed operator+(const Fixed& other) const;
+	Fixed operator-(const Fixed& other) const;
+	Fixed operator*(const Fixed& other) const;
+	Fixed operator/(const Fixed& other) const;
+
+// INCREMENT / DECREMENT OPERATORS
+	Fixed& operator++(void);	// pre-increment
+	Fixed  operator++(int);		// post-increment
+	Fixed& operator--(void);	// pre-decrement
+	Fixed  operator--(int);		// post-decrement
+
+// MIN / MAX static OVERLOADS
+
+	static Fixed&		min(Fixed& a, Fixed& b);
+	static const Fixed&	min(const Fixed& a, const Fixed& b);
+	
+	static Fixed&		max(Fixed& a, Fixed& b);
+	static const Fixed&	max(const Fixed& a, const Fixed& b);
+
+	private:
+		int			_fixedPointValue;
+		static int const	_fractionalBits = 8;
+};
+
+std::ostream& operator<<(std::ostream& out, const Fixed& value);
+
+#endif
+
+/*	Default Constructor: Initializes an object with default values when no arguments are provided
+	Destructor: Cleans up resources and deallocates memory when an object is destroyed
+	Copy Constructor: Creates a new object as a copy of an existing object
+	Copy Assignment Operator: Assigns the contents of one existing object to another existing object
+
+	If your class manages dynamic memory uses (new/delete), must define all three:
+		DESTRUCTOR, COPY CONSTRUCTOR, and COPY ASSIGNMENT OPERATOR.
+	Omitting causes memory leaks or undefined behavior.						*/
