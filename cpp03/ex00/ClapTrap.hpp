218569932e8aea9b4c2527f149f8c4512cff13b0
gomandam <gomandam@student.42madrid.com>
2026-07-27 19:17:07 +0200

    [add] minor arrangement

---
+++ cpp03/ex00/ClapTrap.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gomandam <gomandam@student.42madrid>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/23 21:23:33 by gomandam          #+#    #+#             */
+/*   Updated: 2026/07/23 21:34:00 by gomandam         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef "CLAPTRAP_HPP"
+#define "CLAPTRAP_HPP"
+
+class ClapTrap
+{
+	public:
+		ClapTrap();
+		~ClapTrap();
+
+// ASK AI FOR GUIDED CODING SESSION. start with cpp03/ex00
+
+	private:
+		std::string test = "this is a test.";
+};
+
+
+#endif
